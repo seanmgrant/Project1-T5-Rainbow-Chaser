4a2ed0e218c89dcf95f9314fabddbef290976cf9
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-09-29 19:31:40 -0600

    changing up api to pull specific artist

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,39 +3,16 @@
 var apiKeyMapBox =
   "pk.eyJ1IjoibW1vY2t1czE1IiwiYSI6ImNsOGc2Nnh4cTBlbTUzdm5obHNtMnNuamMifQ.AZXXr5kGKaQPJ9wbx6oIWQ";
 const mainButton = document.querySelector("#rainbowBtn");
-const rainy = "raining-grass.jpeg";
-// const cloudy = 
-// const sunny = 
-// const thunder = 
-// const snow = 
-// const tornado = 
+
 
 
 mainButton.addEventListener("click", () => {
-  alert("This button works!!");
-  console.log("this button works");
 
 });
 
 
 
-// // this will choose and return an image based on current weather conditions which will be passed as argument to function
-// function currentWeather(display) {
-//     switch {
-//         case 'rainy':
 
-//         case 'cloudy'			
-
-//         case 'sunny':
-
-//         case 'windy':
-
-//         case 'lightning':
-
-//         default:
-
-//     }
-// }
 
 function fetchMapBoxMap() {
   fetch(`mapbox://styles/mmockus15/cl8g85w5a000315pmfkxv1wab`).then(
--- assets/js/weatherMusic.js
+++ assets/js/weatherMusic.js
@@ -1,74 +1,51 @@
-var CLIENTID = "yl9kE00201CanbnUZ459WfIu__BwgYur3KhDHfM12B7n0ztlRAlG-XkDMdisCDvc";
-var CLIENTSECRET = "9zw7du1Xkpw55F7L1uoZaYnaTLkmL9IgqC8L5nW9XUooOtd8JbSy3AtJ70CV3z4upXFS15t2_8um6def8iUHjQ";
-var accessToken= "?access_token=ZCPupfL9NToUnxJxrfLyHz2_kisz6QEKCes6-XxoEmn8SWZvRmau4LtsbAsh-JOw";
-var apiUrl = "https://api.genius.com/search";
-var APISong = "https://api.genius.com/songs/";
-var artistID = "395176";
-var maxSong= 2471960; 
-//Max song is 489579 for a fairly safe number. But 2 million songs 
+const container = document.getElementById("container");
+const randomButton = document.getElementById("getRandomSong");
+const randomSong = document.getElementById("randomSong");
+const listOfSongs = document.getElementById("listOfSongs");
 
+const accessToken = "hW3Bng-wVgH972H-hNjQzRyaon_kS2KVssBYMelr6qYupT41QiXmtTJSh1iFZGD-";
+const url = `https://api.genius.com/artists/395176/songs?per_page=50&access_token=${accessToken}`;
+let songsArray = [];
 
-/**
- * Returns a random integer between min (inclusive) and max (inclusive)
- */
-function getRandomInt(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+function buildSongUI(songsArray) {
+  songsArray.forEach(function (song) {
+    console.log(song.title);
+    const li = document.createElement("li");
+    li.textContent = song.title;
+    listOfSongs.appendChild(li);
+  });
 }
 
-//https://api.genius.com/search?q=Kendrick%20Lamar
-var xhr = new XMLHttpRequest(); //XML HTTP Request
-xhr.onreadystatechange = function() {
-	if (xhr.readyState === 4) {
-		if (xhr.status === 200 || xhr.status === 304) {
-			// Success! Do stuff with data.
-			console.log(xhr.responseText); 
-		}
-	}
-};
-xhr.open("GET", APISong+artistID+accessToken, false);
-//xhr.open("GET", apiUrl+accessToken+ '&q=Kendrick%20Lamar', false);
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => {
+    songsArray = response.response.songs;
+    buildSongUI(songsArray);
+  })
+  .catch((err) => console.error(err));
 
-xhr.send(); 
-//console.log(xhr.status);
-//console.log(xhr.statusText);
-demo=xhr.response;
+function getRandomButton() {
+  const song = songsArray[Math.floor(Math.random() * songsArray.length)];
+  randomSong.textContent = song.title;
 
-var json = JSON.parse(demo);
-var song = json['response']['song'];
-
-
-function newRandomSong() {
-	artistID =getRandomInt(1,maxSong);
-	randomSong();
+  console.log(song.title);
 }
 
-function randomSong(){
-	xhr.open("GET", APISong+artistID+accessToken, false);
-	xhr.send(); 
-	demo=xhr.response;
-	
-	while (xhr.status===404){ //Checks if the Random Song Exists
-		artistID =getRandomInt(1,maxSong);
-		xhr.open("GET", APISong+artistID+accessToken, false);
-		xhr.send(); 
-		demo=xhr.response;
-	}
-	
-	json = JSON.parse(demo);
-	song = json['response']['song'];
-	document.getElementById("songImage").innerHTML = "<img src=\""+song['song_art_image_url']+"\"alt=\"Some Awesome Album Art\" style=\"width:200px;height:200px;\">";
-	// I made these pixel values since I'd rather have overlap on a small screen than the image scaled too small 
-	
-	
-	//document.getElementById("song").innerHTML = "SONG: <a href="+song['url']+" >"+song['title'].toUpperCase()+"</a>";
-	document.getElementById("song").innerHTML = "SONG: <a target=\"_blank\" href="+song['url']+" >"+song['title'].toUpperCase()+"</a>";
-	
-	document.getElementById("artist").innerHTML = "ARTIST: <a target=\"_blank\"  href="+song['primary_artist']['url']+">"+song['primary_artist']['name'].toUpperCase()+"</a>";
-}
+randomButton.addEventListener("click", getRandomButton);
+
 function tweetSong(){
 	window.open('https://twitter.com/intent/tweet?hashtags=songs&text=Found a rainbow today! This cool new app called Rainbow Chaser let me drop a pin on the location of the rainbow, and provided me with this cool song as a prize! "'+song['title']+'" by '+song['primary_artist']['name']);
 	
 }
+
+
+
+
+
+
+
 
 
 
@@ -84,22 +61,7 @@ document.getElementById("rainbowTitleBtn").onclick = function () {
 
 
 
-// NOT WORKING- rainbow image array needs to show on page with random button click
-imgArray = [
-	"chool-rainbow.webp",
-	"circle-rainbow.jpeg",
-	"double-rainbow-mountains.jpeg",
-	"rainbow-btn-2.jpeg",
-]
-function randomRainbowImg(imgArray){
-	randomIndex = Math.floor(Math.random() * imgArray.length);
-	selectedImg = imgArray[randomIndex]
-	return selectedImg;
-}
-var rainbowButton = document.getElementById('imgButtonEl');
 
-rainbowButton.addEventListener("click", randomRainbowImg(imgArray) 
-)
 	
 
 
